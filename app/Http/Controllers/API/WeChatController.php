386d6d147f69da4ff1d20b3e96971df4ceb000db
TerryQi <15840345959@139.com>
2018-01-08 01:06:10 +0800

    111

--- app/Http/Controllers/API/WeChatController.php
+++ app/Http/Controllers/API/WeChatController.php
@@ -29,9 +29,38 @@ class WeChatController extends Controller
 
         $app = app('wechat.official_account');
         $app->server->push(function ($message) {
-            return "欢迎关注 overtrue！";
+            Log::info('$message:' + \GuzzleHttp\json_encode($message));
+
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
 
-        return $app->server->serve();
+        $response = $app->server->serve();
+        return $response;
     }
 }
